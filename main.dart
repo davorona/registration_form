dcd2c2b337be3385fdd59ec794506a0a27f2aae1
davorona <96254068+davorona@users.noreply.github.com>
2021-12-29 18:42:45 +0300

    Navigation with Appbar

--- main.dart
+++ main.dart
@@ -1,4 +1,5 @@
 import 'package:demotext/app_bar_screen.dart';
+import 'package:demotext/appbar_navigation.dart';
 import 'package:demotext/auth_screen.dart';
 import 'package:demotext/navigation_drawer_screen.dart';
 import 'package:demotext/registration_form.dart';
@@ -6,5 +7,5 @@ import 'package:demotext/switches_demo_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const RegistrationForm());
+  runApp(const AppBarNavigation());
 }
