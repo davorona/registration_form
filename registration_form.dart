6de1268e540966a6b3a59de7427103e9702f11a7
davorona <96254068+davorona@users.noreply.github.com>
2022-01-01 16:27:38 +0300

    Применение глобальной темы на форму регистрации

--- registration_form.dart
+++ registration_form.dart
@@ -1,3 +1,4 @@
+import 'package:demotext/global_theme_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -19,6 +20,7 @@ class RegistrationForm extends StatelessWidget {
         color: Color(0xFF0079D0));
 
     return MaterialApp(
+      theme: globalTheme(),
       home: Scaffold(
           appBar: AppBar(),
           body: Container(
@@ -33,7 +35,7 @@ class RegistrationForm extends StatelessWidget {
                       children: [
                         Icon(Icons.account_circle),
                         Text('Регистрация',
-                            style: TextStyle(fontSize: 25, color: Color.fromRGBO(0, 0, 0, 6))),
+                            style: Theme.of(context).textTheme.headline6),
                       ],
                     ),
                   ),
@@ -65,7 +67,8 @@ class RegistrationForm extends StatelessWidget {
 
                   ),
                   SizedBox(height: 28),
-                  Text("Вам придет четырехзначный код,который будет вашим паролем"),
+                  Text("Вам придет четырехзначный код,который будет вашим паролем",
+                      style: Theme.of(context).textTheme.bodyText1),
                   SizedBox(height: 10),
                   Text("Чтобы зарегистрироваться введите свой номер телефона и почту"),
                   SizedBox(height: 10),
